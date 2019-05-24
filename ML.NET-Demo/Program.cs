723cd8a3c93b7c29a24067504e00902db189c68d
Leon <leon.id@qq.com>
2019-05-24 11:12:04 +0800

    添加项目文件。

---
+++ ML.NET-Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ML.NET_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
