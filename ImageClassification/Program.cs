3cb41ac9acbb3b8ed3a71abd8a58870e0ca4d684
Leon <leon.id@qq.com>
2019-06-21 15:35:07 +0800

    增加图像分类项目；

---
+++ ImageClassification/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImageClassification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
